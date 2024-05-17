c003a13ae8530cbd5f762d806c95e97515125387
Trevor Watchorn <167261186+atleast14possums@users.noreply.github.com>
2024-05-17 12:20:42 -0500

    Add files via upload
    Initial mdp and analysis files

---
+++ DataCompilation.py
@@ -0,0 +1,47 @@
+# This code is designed for reading multiple text files of the same format and compiling them into one file with the relevant data
+# This can be combined with pyplot to produce a graph of the collective data
+# Reading xvg
+
+import re
+
+# Adding and reading multiple files of one type to a variable for mass analysis
+import os 
+  
+# Folder Path 
+path = "Enter Folder Path"
+  
+# Change the directory 
+
+os.chdir(path) 
+  
+# Read text File 
+  
+  
+def read_text_file(file_path): 
+    with open(file_path, 'r') as f: 
+        print(f.read()) 
+  
+  
+# iterate through all file 
+for file in os.listdir(): 
+    # Check whether file is in text format or not 
+    if file.endswith(".txt"): 
+        file_path = f"{path}\{file}"
+  
+        # call read text file function 
+        read_text_file(file_path) 
+
+with open('file_path', 'r') as f:
+    for line in f:
+        m = re.search(r'xaxis\s+label\s+"([^"]+)"', line)
+        if m is not None:
+            xaxis = m.group(1)
+        m = re.search(r'yaxis\s+label\s+"([^"]+)"', line)
+        if m is not None:
+            yaxis = m.group(1)
+        if line.startswith('@ s0 legend'):
+            break
+    df = pd.read_csv(f, names=[xaxis, yaxis], delim_whitespace=True)
+    f.close()
+    
+print(df)        
---
+++ README.md
@@ -0,0 +1,7 @@
+README
+
+The files in this repository are going to be primarily simple anaylsis and input files.
+
+mdawh is an mdp file setup for PMF analysis, the mdp will provide a consistent temperature and pressure based off your inputs, this mdp does not make use of simulated annealing. 
+
+DataCompilation is a simple python program to combine multiple output text files of the same format (generally .xvg) into one file for anaylsis, particularly useful with simulations that are paused and restarted. 
